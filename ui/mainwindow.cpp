b846e4bfc07a3539b3a9f0bdf15b5ed1b2b2ca25
sh1erkeh <arseniy29042006@gmail.com>
2025-02-03 13:23:14 +0300

    Changed project layout

--- ui/mainwindow.cpp
+++ ui/mainwindow.cpp
@@ -1,30 +1,36 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+
 #include <QComboBox>
 #include <QWidget>
 #include <QVBoxLayout>
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-    , row(5), col(0)
-{
+        : QMainWindow(parent), ui(new Ui::MainWindow), row(5), col(0) {
     ui->setupUi(this);
+
     switch_1 = ui->pushButton;
     show_1 = ui->show_3;
+
     switch_2 = ui->pushButton_2;
     show_2 = ui->show_4;
+
     switch_3 = ui->pushButton_3;
     show_3 = ui->show_5;
+
     switch_4 = ui->pushButton_4;
     show_4 = ui->show_6;
+
     show_2->hide();
     show_3->hide();
     show_4->hide();
+
     ui->widget_5->hide();
     ui->widget_3->show();
     ui->widget_8->show();
     ui->widget_6->hide();
     ui->widget_9->hide();
+
     switch_1->setStyleSheet("QPushButton {""background-color: rgb(40,40,40);"
                             "border-top-right-radius: 15px;"
                             "border-top-left-radius: 15px;""}");
@@ -37,33 +43,38 @@ MainWindow::MainWindow(QWidget *parent)
     switch_4->setStyleSheet("QPushButton {""background-color: rgb(60,60,60);"
                             "border-top-right-radius: 15px;"
                             "border-top-left-radius: 15px;""}");
+
     connect(switch_1, &QPushButton::clicked, this, &MainWindow::onSwitch1Clicked);
     connect(switch_2, &QPushButton::clicked, this, &MainWindow::onSwitch2Clicked);
     connect(switch_3, &QPushButton::clicked, this, &MainWindow::onSwitch3Clicked);
     connect(switch_4, &QPushButton::clicked, this, &MainWindow::onSwitch4Clicked);
+
     connect(ui->pushButton_12, &QPushButton::clicked, this, &MainWindow::onButton12Clicked);
     connect(ui->pushButton_13, &QPushButton::clicked, this, &MainWindow::onButton13Clicked);
     connect(ui->pushButton_16, &QPushButton::clicked, this, &MainWindow::onButton16Clicked);
+
     grid_layout=qobject_cast<QGridLayout*>(ui->show_6->layout());
     connect(ui->pushButton_6, &QPushButton::clicked, this, &MainWindow::creating_new_subject);
     // Изначально скрываем все виджеты
-
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
+
 void MainWindow::creating_new_subject(){
     QWidget *new_widget = new QWidget;
     new_widget->setStyleSheet("background-color:rgb(80,80,80);"
                               "border-radius: 15px;");
+
     QPlainTextEdit *input_subj = new QPlainTextEdit(new_widget);
     new_widget->setMaximumHeight(50);
     input_subj->setStyleSheet("background-color:rgb(255,255,255);"
                               "border-radius: 10px;");
+
     input_subj->setMaximumHeight(40);
     QVBoxLayout *layout = new QVBoxLayout(new_widget);
+
     layout->addWidget(input_subj);
     new_widget->setLayout(layout);
     grid_layout->addWidget(new_widget,row,col);
@@ -73,10 +84,10 @@ void MainWindow::creating_new_subject(){
 }
 
 // Слот для кнопки switch_1
-void MainWindow::onSwitch1Clicked()
-{
-    hideAllWidgets();  // Скрываем все виджеты
-    show_1->show();  // Показываем виджет show_1
+void MainWindow::onSwitch1Clicked() {
+    hideAllWidgets();
+    show_1->show();
+
     switch_1->setStyleSheet("QPushButton {""background-color: rgb(40,40,40);"
                             "border-top-right-radius: 15px;"
                             "border-top-left-radius: 15px;""}");
@@ -92,13 +103,14 @@ void MainWindow::onSwitch1Clicked()
 }
 
 // Слот для кнопки switch_2
-void MainWindow::onSwitch2Clicked()
-{
-    hideAllWidgets();  // Скрываем все виджеты
-    show_2->show();  // Показываем виджет show_2
+void MainWindow::onSwitch2Clicked() {
+    hideAllWidgets();
+    show_2->show();
+
     ui->plainTextEdit_13->setEnabled(true);
     ui->plainTextEdit_8->setEnabled(true);
     ui->plainTextEdit_9->setEnabled(true);
+
     switch_1->setStyleSheet("QPushButton {""background-color: rgb(60,60,60);"
                             "border-top-right-radius: 15px;"
                             "border-top-left-radius: 15px;""}");
@@ -114,14 +126,15 @@ void MainWindow::onSwitch2Clicked()
 }
 
 // Слот для кнопки switch_3
-void MainWindow::onSwitch3Clicked()
-{
-    hideAllWidgets();  // Скрываем все виджеты
-    show_3->show();  // Показываем виджет show_3
+void MainWindow::onSwitch3Clicked() {
+    hideAllWidgets();
+    show_3->show();
+
     ui->plainTextEdit_10->setEnabled(true);
     ui->plainTextEdit_11->setEnabled(true);
     ui->plainTextEdit_12->setEnabled(true);
     ui->plainTextEdit_10->setFocus();
+
     switch_1->setStyleSheet("QPushButton {""background-color: rgb(60,60,60);"
                             "border-top-right-radius: 15px;"
                             "border-top-left-radius: 15px;""}");
@@ -138,12 +151,13 @@ void MainWindow::onSwitch3Clicked()
 }
 
 // Слот для кнопки switch_4
-void MainWindow::onSwitch4Clicked()
-{
-    hideAllWidgets();  // Скрываем все виджеты
-    show_4->show();  // Показываем виджет show_4
+void MainWindow::onSwitch4Clicked() {
+    hideAllWidgets();
+    show_4->show();
+
     ui->plainTextEdit->setEnabled(true);
     ui->plainTextEdit->setFocus();
+
     switch_1->setStyleSheet("QPushButton {""background-color: rgb(60,60,60);"
                             "border-top-right-radius: 15px;"
                             "border-top-left-radius: 15px;""}");
@@ -159,15 +173,14 @@ void MainWindow::onSwitch4Clicked()
 }
 
 // Метод для скрытия всех виджетов
-void MainWindow::hideAllWidgets()
-{
+void MainWindow::hideAllWidgets() {
     show_1->hide();
     show_2->hide();
     show_3->hide();
     show_4->hide();
 }
-void MainWindow::onButton12Clicked()
-{
+
+void MainWindow::onButton12Clicked() {
     ui->widget_3->hide();
     ui->widget_8->hide();
     ui->widget->hide();
@@ -175,8 +188,8 @@ void MainWindow::onButton12Clicked()
     ui->widget_9->show();
     ui->widget_6->show();
 }
-void MainWindow::onButton13Clicked()
-{
+
+void MainWindow::onButton13Clicked() {
     ui->widget_3->show();
     ui->widget->show();
     ui->widget_5->hide();
@@ -186,8 +199,6 @@ void MainWindow::onButton13Clicked()
 
 }
 
-void MainWindow::onButton16Clicked()
-{
+void MainWindow::onButton16Clicked() {
     close();
-
 }
--- ui/mainwindow.h
+++ ui/mainwindow.h
@@ -8,16 +8,17 @@
 #include <QPlainTextEdit>
 #include <QGridLayout>
 #include <QVBoxLayout>
+
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui { 
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 private slots:
     void onSwitch1Clicked();
@@ -30,20 +31,20 @@ private slots:
     void hideAllWidgets();
     void creating_new_subject();
 private:
-    Ui::MainWindow *ui;
-    QPushButton *switch_1;
-    QPushButton *switch_2;
-    QPushButton *switch_3;
-    QPushButton *switch_4;
-    QWidget *show_1;
-    QWidget *show_2;
-    QWidget *show_3;
-    QWidget *show_4;
-    QWidget *new_widget;
-    QComboBox *comboBox;
-    QPlainTextEdit *input_subj;
-    QGridLayout *grid_layout;
-    QVBoxLayout *layout;
+    Ui::MainWindow* ui;
+    QPushButton* switch_1;
+    QPushButton* switch_2;
+    QPushButton* switch_3;
+    QPushButton* switch_4;
+    QWidget* show_1;
+    QWidget* show_2;
+    QWidget* show_3;
+    QWidget* show_4;
+    QWidget* new_widget;
+    QComboBox* comboBox;
+    QPlainTextEdit* input_subj;
+    QGridLayout* grid_layout;
+    QVBoxLayout* layout;
     int row,col;
 };
 
