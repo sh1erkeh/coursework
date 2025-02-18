e758798bcbddc812b59a70358beb1862dd4e5cfc
sh1erkeh <arseniy29042006@gmail.com>
2025-02-18 13:47:16 +0300

    made db-handler more flexible (C++23 required)

--- lib/DB-handlers/include/handler.hpp
+++ lib/DB-handlers/include/handler.hpp
@@ -1,20 +1,44 @@
 #pragma once
 
 #include <sqlite3.h>
+#include <iostream>
 
+template<typename Fetcher>
 class Handler {
 private:
     sqlite3* DB;
-    static int log(void*, int, char**, char**);
 
-    Handler(const char*);
-    ~Handler();
+    Handler(const char* db_name) {
+        if (sqlite3_open(db_name, &DB)) {
+            std::cerr << "Can't open database: " << sqlite3_errmsg(DB) << std::endl;
+        } else {
+            std::cerr << "Opened database successfully" << std::endl;
+        }
+    }
+
+    ~Handler() {
+        sqlite3_close(DB);
+    }
 
     Handler(const Handler&) = delete;
     Handler& operator=(const Handler&) = delete;
 public:
-    static Handler& Instance(const char*);
+    static Handler& Instance(const char* db_name) {
+        static Handler inst(db_name);
+        return inst;
+    }
 
-    void execute(const char*);
+    void execute(const char* query) {
+        char* errorMsg;
+        const char* data = "Fetcher called";
+
+        // TODO: remove bad c-style cast
+        if (sqlite3_exec(DB, query, &Fetcher::operator(), (void*)data, &errorMsg) != SQLITE_OK) {
+            std::cerr << "SQL error: " << errorMsg << std::endl;
+            sqlite3_free(errorMsg);
+        } else {
+            std::cerr << "Query executed successfully" << std::endl;
+        } 
+    }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include "handler.hpp"
 
+#include <iostream>
+
+struct Log {
+    static int operator()(void* data, int argc, char** argv, char** colName) {
+        std::cerr << static_cast<const char*>(data) << ":\n";
+        for (int i = 0; i < argc; i++) {
+            std::cerr << colName[i] << " = " << (argv[i] ? argv[i] : "NULL") << '\n';
+        }
+        std::cerr << std::endl;
+        return 0;
+    }
+};
 int main() {
-    Handler& h = Handler::Instance("test.db");
+    Handler<Log>& h = Handler<Log>::Instance("test.db");
+    const char* sql = "CREATE TABLE COMPANY("  \
+      "ID INT PRIMARY KEY     NOT NULL," \
+      "NAME           TEXT    NOT NULL," \
+      "AGE            INT     NOT NULL," \
+      "ADDRESS        CHAR(50)," \
+      "SALARY         REAL );";
+    h.execute(sql);
+    sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+         "VALUES (1, 'Paul', 32, 'California', 20000.00 ); " \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
+         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" \
+         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
+         "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+    h.execute(sql);
+    sql = "SELECT * from COMPANY";
+    h.execute(sql);
 }
