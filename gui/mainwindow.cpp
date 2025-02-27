454207c29d91e998dbf5553a7d9545edf4a58bc7
Alex Andreev <sashasasha192021@gmail.com>
2025-02-27 15:33:04 +0300

    renew design
    renew design

---
+++ gui/mainwindow.cpp
@@ -0,0 +1,513 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QPushButton>
+#include <QWidget>
+#include <QComboBox>
+#include <QPlainTextEdit>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QSpacerItem>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QSpinBox>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+#include <QDir>
+#include <QPalette>
+#include <QGraphicsDropShadowEffect>
+#include <QLabel>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow), count(2), row(0)
+{
+    ui->setupUi(this);
+    ui->widget_5->hide();
+    ui->widget_3->show();
+    ui->widget_8->show();
+    ui->widget_9->hide();
+    ui->widget_6->hide();
+    int row=0;
+    QDir dir;
+    QString path = dir.currentPath() + "/database";
+
+    if (!dir.exists(path)) {
+        dir.mkpath(path);
+    }
+
+    QString dbPath = path + "/data.db";
+
+    QSqlDatabase db = QSqlDatabase::addDatabase(("QSQLITE"));
+    db.setDatabaseName(dbPath);
+
+    if (!db.open()){
+        qDebug() <<"Ошибка открытия базы данных: "<< db.lastError().text();
+        return;
+    }
+    QSqlQuery query;
+
+    QString dropTableQuery = "DROP TABLE IF EXISTS text_data";
+    if (!query.exec(dropTableQuery)) {
+        qDebug() << "Ошибка удаления таблицы: " << query.lastError().text();
+    } else {
+        qDebug() << "Старая таблица text_data успешно удалена!";
+    }
+
+    QString createTableQuery =
+        "CREATE TABLE IF NOT EXISTS text_data ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "page INTEGER, "
+        "subject_name TEXT,"
+        "amount TEXT)";
+
+    if (!query.exec(createTableQuery)) {
+        qDebug() <<"Ошибка создания таблицы: " << query.lastError().text();
+    }else{
+        qDebug() << "База данных успешно инициализирована!";
+        qDebug() <<dbPath;
+    }
+    QSqlQuery query1;
+    QString createTableSubjects =
+        "CREATE TABLE IF NOT EXISTS subjects ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "name TEXT UNIQUE)";
+
+    if (!query1.exec(createTableSubjects)) {
+        qDebug() <<"Ошибка создания таблицы: " << query1.lastError().text();
+    }else{
+        qDebug() << "База данных успешно инициализирована!";
+    }
+
+
+    //связываю свитчи с переключением окна
+    connect(ui->pushButton_12, &QPushButton::clicked, this, &MainWindow::onButton12Clicked);
+    connect(ui->pushButton_13, &QPushButton::clicked, this, &MainWindow::onButton13Clicked);
+    connect(ui->pushButton_16, &QPushButton::clicked, this, &MainWindow::onButton16Clicked);
+
+    QGraphicsDropShadowEffect *effect = new QGraphicsDropShadowEffect;
+    effect->setBlurRadius(10);
+    effect->setOffset(0);
+    effect->setColor(QColor(0,0,0,100));
+    ui->widget_20->setGraphicsEffect(effect);
+
+    QGraphicsDropShadowEffect *effect1 = new QGraphicsDropShadowEffect;
+    effect1->setBlurRadius(10);
+    effect1->setOffset(0);
+    effect1->setColor(QColor(0,0,0,100));
+
+    ui->widget_13->setGraphicsEffect(effect1);
+
+    //беру скролл виджет из гуишки чтобы получить из него лейаут и потом в лейаут пихаю элементы
+    QWidget *scrollWidget = ui->scrollArea->widget();
+    if (!scrollWidget){
+        scrollWidget = new QWidget;
+        ui->scrollArea->setWidget(scrollWidget);
+        ui->scrollArea->setWidgetResizable(true);
+    }
+    //получаю лейаут из скролл виджета
+    QVBoxLayout* scrollLayout = qobject_cast<QVBoxLayout*>(scrollWidget->layout());
+    if (!scrollLayout){
+        scrollLayout = new QVBoxLayout(scrollWidget);
+        scrollWidget->setLayout(scrollLayout);
+    }
+    ui->switch_area->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    //создаем спейсер в скролл арея чтобы все элементы сверху были
+    spacer = new QSpacerItem(20,40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    scrollLayout->addItem(spacer);
+    QWidget *firstPage = ui->stackedWidget->widget(0);
+    QVBoxLayout *layout1 = new QVBoxLayout(firstPage);
+    layout1->addWidget(ui->show_8);
+    layout1->setContentsMargins(0,0,0,0);
+    QWidget *firstPage1 = ui->stackedWidget->widget(1);
+    QVBoxLayout *layout2 = new QVBoxLayout(firstPage1);
+    layout2->setContentsMargins(0,0,0,0);
+    layout2->addWidget(ui->show_6);
+    buttonToIndex[ui->pushButton_4] = 0;
+    buttonToIndex[ui->pushButton_17] = 1;
+    int count = 3;
+    connect(ui->pushButton_5, &QPushButton::clicked, this, [this, count]()mutable {
+        onSwitch4Clicked(QString::number(count));
+        count++;
+    });
+
+    QWidget *scrollWidget_4 = ui->scrollArea_3->widget();
+    if (!scrollWidget_4){
+        scrollWidget_4 = new QWidget;
+        ui->scrollArea_3->setWidget(scrollWidget_4);
+        ui->scrollArea_3->setWidgetResizable(true);
+    }
+
+    QVBoxLayout* scrollLayout_4 = qobject_cast<QVBoxLayout*>(scrollWidget_4->layout());
+    if (!scrollLayout_4){
+        scrollLayout_4 = new QVBoxLayout(scrollWidget_4);
+        scrollWidget_4->setLayout(scrollLayout_4);
+    }
+
+    spacer_4 = new QSpacerItem(20,40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+    scrollLayout_4->addItem(spacer_4);
+
+    ui->switch_area->horizontalScrollBar()->setValue(ui->switch_area->horizontalScrollBar()->maximum());
+
+    ui->scrollArea_3->verticalScrollBar()->setValue(ui->scrollArea_3->verticalScrollBar()->maximum());
+    ui->scrollArea->verticalScrollBar()->setValue(ui->scrollArea->verticalScrollBar()->maximum());
+    connect(ui->pushButton_6, &QPushButton::clicked, this, [this, scrollLayout]() {
+        addNewWidget(scrollLayout, this->ui->scrollArea, this->ui->pushButton_6);
+    });
+    connect(ui->pushButton_14, &QPushButton::clicked, this, [this, scrollLayout_4]() {
+        addNewWidget(scrollLayout_4, this->ui->scrollArea_3, this->ui->pushButton_14);
+    });
+    connect(ui->pushButton_4, &QPushButton::clicked, this, &MainWindow::switchPage);
+    connect(ui->pushButton_17, &QPushButton::clicked, this, &MainWindow::switchPage);
+    connect(ui->saveButton, &QPushButton::clicked, this, &MainWindow::on_saveButton_clicked);
+    connect(ui->pushButton_15, &QPushButton::clicked, this, &MainWindow::on_saveButton_clicked);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::on_saveButton_clicked() {
+    QSqlDatabase db = QSqlDatabase::database();
+
+    if (!db.isOpen()) {
+        qDebug() << "Ошибка: база данных не открыта";
+        return;
+    }
+
+    QSqlQuery query(db);
+    QSqlQuery query1(db);
+    QSqlQuery getIdQuery(db);
+
+    // Очистка таблицы text_data
+    if (!query.exec("DELETE FROM text_data")) {
+        qDebug() << "Ошибка очистки таблицы" << query.lastError().text();
+        return;
+    } else {
+        qDebug() << "Таблица text_data успешно очищена";
+    }
+    query.clear();
+    query.prepare("INSERT INTO text_data (page, subject_name, amount) VALUES (:page, :subject_name, :amount)");
+    query1.prepare("INSERT INTO subjects (name) VALUES (:name)");
+
+    int pageCount = ui->stackedWidget->count();
+    for (int i = 0; i < pageCount; ++i) {
+        QWidget *page = ui->stackedWidget->widget(i);
+
+        QList<QPlainTextEdit *> textEdits = page->findChildren<QPlainTextEdit *>();
+        QList<QSpinBox *> spinBoxes = page->findChildren<QSpinBox *>();
+
+        QStringList subjectList;
+        QStringList amountList;
+
+        for (int j = 0; j < textEdits.size(); ++j) {
+            QPlainTextEdit *textEdit = textEdits[j];
+            QSpinBox *textSpin = spinBoxes[j];
+
+            if (textEdit && textSpin) {
+                QString content = textEdit->toPlainText().trimmed();
+                int spin = textSpin->value();
+
+                if (content.isEmpty()) {
+                    qDebug() << "Пропущен пустой предмет";
+                    continue;
+                }
+
+                // Проверяем, существует ли предмет в базе
+                getIdQuery.prepare("SELECT id FROM subjects WHERE name = :name");
+                getIdQuery.bindValue(":name", content);
+
+                if (!getIdQuery.exec()) {
+                    qDebug() << "Ошибка поиска предмета: " << getIdQuery.lastError().text();
+                    continue;
+                }
+
+                if (!getIdQuery.next()) {
+                    // Если предмет не найден, добавляем его
+                    query1.bindValue(":name", content);
+                    if (!query1.exec()) {
+                        qDebug() << "Ошибка сохранения предмета: " << query1.lastError().text();
+                        continue;
+                    }
+                }
+
+                // Добавляем в списки для группового сохранения
+                subjectList.append(content);
+                amountList.append(QString::number(spin));
+            }
+        }
+
+        if (!subjectList.isEmpty() && !amountList.isEmpty()) {
+            QString subject_list=subjectList.join(",");
+            QString amount_list = amountList.join(",");
+            query.bindValue(":page", i);
+            query.bindValue(":subject_name", subject_list);
+            query.bindValue(":amount", amount_list);
+
+            if (!query.exec()) {
+                qDebug() << "Ошибка сохранения text_data: " << query.lastError().text();
+            }
+        } else {
+            qDebug() << "Пропущена страница " << i << ", так как нет данных для сохранения.";
+        }
+    }
+}
+void MainWindow::addNewWidget(QVBoxLayout *scrollLayout, QScrollArea *scrollArea, QPushButton *pushButton_6) {
+
+    if (row >= 35) {
+        return;
+    }
+    QPalette palette;
+    palette.setColor(QPalette::Text, Qt::black);
+
+    // Создаем новый виджет
+    QWidget *newWidget = new QWidget();
+    newWidget->setStyleSheet("background-color: rgb(80, 80, 80); border-radius: 15px;");
+
+    // Создаем текстовое поле
+    QPlainTextEdit *textEdit = new QPlainTextEdit(newWidget);
+    textEdit->setStyleSheet("background-color: rgb(255, 255, 255); border-radius: 10px;color: black; padding:7px");
+
+
+    textEdit->setMinimumHeight(40);
+    //создаем виджет для спин бокса
+    QWidget *widget_spinbox = new QWidget();
+    widget_spinbox->setStyleSheet("background-color:rgb(200,200,200); border-radius: 10px;");
+    widget_spinbox->setFixedSize(50,40);
+
+    //создаем спинбокс и грид для виджета под спинбокс
+    QSpinBox *spinbox = new QSpinBox;
+    QHBoxLayout *widget_spinbox_layout = new QHBoxLayout(widget_spinbox);
+    widget_spinbox_layout->addWidget(spinbox);
+    widget_spinbox->setLayout(widget_spinbox_layout);
+    widget_spinbox_layout->setContentsMargins(7,0,3,0);
+
+    spinbox->setMinimumHeight(30);
+    spinbox->setMinimumWidth(40);
+    spinbox->setStyleSheet(ui->spinBox_3->styleSheet());
+    spinbox->setRange(1,4);
+    spinbox->setPalette(palette);
+    // Устанавливаем layout для нового виджета
+    QHBoxLayout *newWidgetLayout = new QHBoxLayout(newWidget);
+    QSpacerItem *spacer_button= new QSpacerItem(10,10, QSizePolicy::Minimum, QSizePolicy::Minimum);
+    newWidgetLayout->setContentsMargins(5,5,5,5);
+    newWidgetLayout->setSpacing(20);
+    newWidgetLayout->addWidget(widget_spinbox);
+    newWidgetLayout->addWidget(textEdit);
+    newWidget->setLayout(newWidgetLayout);
+    newWidget->setFixedHeight(50);
+    // Добавляем новый виджет в layout scrollWidget
+
+    scrollLayout->setSpacing(0);
+    scrollLayout->insertWidget(scrollLayout->indexOf(pushButton_6),newWidget);
+    if (checking_label[pushButton_6]==0 && pushButton_6!=ui->pushButton_14){
+        QWidget *widget_tip = new QWidget;
+        widget_tip->setFixedHeight(30);
+        QSpacerItem *gap = new QSpacerItem(10,10, QSizePolicy::Expanding, QSizePolicy::Minimum);
+        QSpacerItem *gap1 = new QSpacerItem(10,10, QSizePolicy::Minimum, QSizePolicy::Minimum);
+        QLabel *amount = new QLabel("Кол-во");
+        amount->setStyleSheet("color:rgb(120,120,120);");
+        QLabel *pairs = new QLabel("Название предмета");
+        QHBoxLayout *tip_layout = new QHBoxLayout;
+        pairs->setStyleSheet("color:rgb(120,120,120);");
+
+        tip_layout->setContentsMargins(4,5,4,0);
+        // tip_layout->addItem(gap);
+        tip_layout->addWidget(amount);
+        tip_layout->addItem(gap1);
+        tip_layout->addWidget(pairs);
+        tip_layout->addItem(gap);
+        widget_tip->setLayout(tip_layout);
+
+        checking_label[pushButton_6]=1;
+
+        scrollLayout->insertWidget(scrollLayout->indexOf(newWidget), widget_tip);
+    }
+    scrollLayout->insertItem(scrollLayout->indexOf(pushButton_6),spacer_button);
+    scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+    // Увеличиваем количество виджетов
+    row++;
+
+    // Обновляем прокрутку
+    scrollArea->verticalScrollBar()->setValue(scrollArea->verticalScrollBar()->maximum());
+}
+// Слот для кнопки switch_4
+void MainWindow::onSwitch4Clicked(const QString &buttonText)//получается мне надо при создании page в основном экране также создавать page_alter с с пустями колонками пар
+//во втором окне
+{   if (count>=80){
+        return;}
+    row = 0;
+    //создаю новую кнопку
+    QPushButton *new_switch = new QPushButton(buttonText);
+    new_switch->setStyleSheet("QPushButton {""background-color: rgb(40,40,40);"
+                         "border-top-right-radius: 15px;"
+                          "border-top-left-radius: 15px;"
+                          "color: white;}"
+                          "QPushButton:hover {background-color: rgb(60,60,60);}"
+                          "QPushButton:pressed {background-color: rgb(30,30,30);}");
+    new_switch->setFixedSize(60,40);
+
+    QWidget *switch_scroll_widget = ui->switch_area->widget();
+    if (!switch_scroll_widget){
+        switch_scroll_widget = new QWidget;
+        ui->switch_area->setWidget(switch_scroll_widget);
+        ui->switch_area->setWidgetResizable(true);
+    }
+
+    QWidget *original = ui->show_8; // Исходный виджет
+    QWidget *group = new QWidget;
+    QWidget *page = new QWidget(); // Создаём копию
+
+    // Копируем свойства
+    page->setGeometry(original->geometry());
+    page->setStyleSheet(original->styleSheet());
+    ui->stackedWidget->addWidget(page);
+
+    QGraphicsDropShadowEffect *effect = new QGraphicsDropShadowEffect;
+    effect->setBlurRadius(10);
+    effect->setOffset(0);
+    effect->setColor(QColor(0,0,0,100));
+
+    group->setFixedSize(100,30);
+    group->setContentsMargins(0,0,0,0);
+    group->setGraphicsEffect(effect);
+    group->setStyleSheet(ui->widget_13->styleSheet());
+
+
+    QStringList groupList;
+    groupList.append(buttonText);
+    groupList.append(" группа");
+    QLabel *group_label = new QLabel(groupList.join(""));
+    QGridLayout *group_layout = new QGridLayout;
+    group_layout->setContentsMargins(0,0,0,0);
+    group_layout->addWidget(group_label);
+    group_layout->setAlignment(Qt::AlignCenter);
+    group->setLayout(group_layout);
+    //Теперь братик page создан и я постараюсь в него впихнуть здесь дочерние элементы по типу push_button и plaintextedit, scroll layout еще
+    QScrollArea *page_scroll_area  = new QScrollArea;
+    QPushButton *page_button_add = new QPushButton("Добавить пару");
+    QPushButton *page_button_save = new QPushButton("Сохранить");
+
+    QWidget *page_scrollWidget = page_scroll_area->widget();
+    if (!page_scrollWidget){
+        page_scrollWidget = new QWidget;
+        page_scroll_area->setWidget(page_scrollWidget);
+        page_scroll_area->setWidgetResizable(true);
+    }
+    page_scroll_area->setParent(page);
+    page_scroll_area->setWidgetResizable(true);
+    page_scroll_area->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
+    page_scroll_area->setStyleSheet(ui->scrollArea_3->styleSheet());
+    page_scroll_area->setGeometry(ui->scrollArea_3->geometry());
+
+
+    page_scroll_area->setStyleSheet(
+        "QScrollBar:vertical {"
+        "    border: none;"
+        "    background: #2E2E2E;"
+        "    width: 10px;"
+        "    margin: 0px 0px 0px 0px;"
+        "}"
+        "QScrollBar::handle:vertical {"
+        "    background: #555555;"
+        "    min-height: 20px;"
+        "    border-radius: 5px;"
+        "}"
+        "QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {"
+        "    border: none;"
+        "    background: none;"
+        "    height: 0px;"
+        "}"
+        "QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {"
+        "    background: none;"
+        "}");
+
+    QGridLayout *page_vertical_layout = qobject_cast<QGridLayout*>(page->layout());
+    if (!page_vertical_layout){
+        page_vertical_layout = new QGridLayout(page);
+        page->setLayout(page_vertical_layout);
+    }
+    page_vertical_layout->addWidget(page_scroll_area);
+    page_vertical_layout->setContentsMargins(20,20,20,20);
+    //получаю лейаут из скролл виджета
+    QVBoxLayout *page_layout = qobject_cast<QVBoxLayout*>(page_scrollWidget->layout());
+    if (!page_layout){
+        page_layout = new QVBoxLayout(page_scrollWidget);
+        page_scrollWidget->setLayout(page_layout);
+    }
+
+    page_button_add->setFixedHeight(40);
+    page_button_add->setStyleSheet(ui->pushButton_14->styleSheet());
+    page_button_save->setFixedHeight(30);
+    page_button_save->setStyleSheet(ui->saveButton->styleSheet());
+
+    QSpacerItem *spacer = new QSpacerItem(0,0, QSizePolicy::Minimum, QSizePolicy::Minimum);
+    page_layout->addItem(spacer);
+    page_layout->setContentsMargins(12,12,12,12);
+    page_layout->addWidget(group);
+    page_layout->addWidget(page_button_add);
+    QSpacerItem *spacer2 = new QSpacerItem(10,10, QSizePolicy::Minimum, QSizePolicy::Minimum);
+    page_layout->addItem(spacer2);
+    page_layout->addWidget(page_button_save);
+    QSpacerItem *spacer3 = new QSpacerItem(10,10, QSizePolicy::Expanding, QSizePolicy::Expanding);
+    page_layout->addItem(spacer3);
+    page_scroll_area->verticalScrollBar()->setValue(page_scroll_area->verticalScrollBar()->maximum());
+    ui->stackedWidget->addWidget(page);
+    //добавляю скролл area для page
+    int index = ui->stackedWidget->indexOf(page);
+    buttonToIndex[new_switch] = index;
+    qDebug()<<index;
+    page->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    QHBoxLayout *switch_layout = qobject_cast<QHBoxLayout*>(switch_scroll_widget->layout());
+    switch_layout->insertWidget(switch_layout->indexOf(ui->pushButton_5), new_switch);
+    (count)++;
+
+    connect(new_switch, &QPushButton::clicked, this, &MainWindow::switchPage);
+
+    connect(page_button_add, &QPushButton::clicked, this, [this, page_layout, page_scroll_area, page_button_add]() {
+        addNewWidget(page_layout, page_scroll_area, page_button_add);
+    });
+
+    connect(page_button_save, &QPushButton::clicked, this, &MainWindow::on_saveButton_clicked);
+
+}
+
+void MainWindow::switchPage(){
+    QPushButton *button = qobject_cast<QPushButton*>(sender());
+    if (button&&buttonToIndex.contains(button)){
+        int index = buttonToIndex[button];
+        qDebug()<<index;
+        ui->stackedWidget->setCurrentIndex(index);
+    }
+}
+// Метод для скрытия всех виджетов
+void MainWindow::onButton12Clicked()
+{
+    ui->widget->hide();
+    ui->widget_5->show();
+    ui->widget_3->hide();
+    ui->widget_8->hide();
+    ui->widget_9->show();
+    ui->widget_6->show();
+}
+void MainWindow::onButton13Clicked()
+{
+    ui->widget->show();
+    ui->widget_5->hide();
+    ui->widget_3->show();
+    ui->widget_8->show();
+    ui->widget_9->hide();
+    ui->widget_6->hide();
+
+}
+
+void MainWindow::onButton16Clicked()
+{
+    close();
+
+}
---
+++ gui/mainwindow.h
@@ -0,0 +1,120 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QWidget>
+#include <QComboBox>
+#include <QPlainTextEdit>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QSpacerItem>
+#include <QStackedWidget>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QSpinBox>
+#include <QMap>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDir>
+#include <QPalette>
+#include <QGraphicsDropShadowEffect>
+#include <QLabel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+    void onSwitch4Clicked(const QString &buttonText);
+    void onButton12Clicked();
+    void onButton13Clicked();
+    void onButton16Clicked();
+    void addNewWidget(QVBoxLayout *scrollLayout, QScrollArea *scrollArea, QPushButton *pushButton_6);
+    void switchPage();
+    void on_saveButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QPushButton *switch_1;
+    QPushButton *switch_2;
+    QPushButton *switch_3;
+    QPushButton *switch_4;
+    QWidget *show_1;
+    QWidget *show_2;
+    QWidget *show_3;
+    QWidget *show_4;
+    QWidget *new_widget;
+    QWidget *scrollWidget;
+    QComboBox *comboBox;
+    QPlainTextEdit *input_subj;
+    QGridLayout *grid_layout;
+    QVBoxLayout *layout;
+    QSpacerItem *spacer;
+    QSpacerItem *spacer1;
+    QScrollArea *scrollArea;
+    QVBoxLayout *scrollLayout;
+    QHBoxLayout *widget_spinbox_layout;
+    QHBoxLayout *newWidgetLayout;
+    QWidget *widget_spinbox;
+    QSpinBox *spinbox;
+    QPushButton *new_switch;
+    QWidget *switch_scroll_widget;
+    QHBoxLayout *switch_layout;
+    QSpacerItem *spacer_switch;
+    QVBoxLayout *buttonsLayout;
+    QStackedWidget *stackedWidget;
+    QMap<QPushButton*, int> buttonToIndex;
+    QMap<QPushButton*, int> checking_label;
+    QPushButton *push_button_pairs;
+    QScrollArea *page_scroll_area;
+    QPushButton *page_button_add;
+    QVBoxLayout *page_layout;
+    QWidget *page_scrollWidget;
+    QSpacerItem *spacer2;
+    QSpacerItem *spacer_4;
+    QVBoxLayout *layout1;
+    QVBoxLayout *layout2;
+    QWidget *firstPage1;
+    QWidget *firstPage;
+    QWidget *scrollWidget_4;
+    QVBoxLayout* scrollLayout_4;
+    QWidget *container;
+    QPlainTextEdit *textEdit;
+    QSpinBox *textSpin;
+    QList<QWidget *> containers;
+    QString *subject_names;
+    QString *subject_spins;
+    QWidget *page;
+    QStringList subjectList;
+    QStringList amountList;
+    QPalette *palette;
+    QPushButton *page_button_save;
+    QGraphicsDropShadowEffect *effect;
+    QWidget *group;
+    QGridLayout *group_layout;
+    QLabel *group_label;
+    QGraphicsDropShadowEffect *effect1;
+    QSpacerItem *spacer_button;
+    QWidget *widget_tip;
+    QLabel *amount;
+    QLabel *pairs;
+    QHBoxLayout *tip_layout;
+    QSpacerItem *gap;
+    QSpacerItem *gap1;
+    QSpacerItem *spacer3;
+
+
+    int row=0,col, count=2;
+};
+
+#endif // MAINWINDOW_H
